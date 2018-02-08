b89b1d878c55c794b478ee333348f6c39e2fb225
kierstengyra <ramoskierstengyra@gmail.com>
2018-02-08 21:23:41 +0800

    3D Hologram Display
    Back-up only.

---
+++ AR Trial/Assets/Vuforia/Scripts/RotateScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateScript : MonoBehaviour {
+	private float sensitivity;
+	private Vector3 mouseReference;
+	private Vector3 mouseOffset;
+	private Vector3 rotation;
+	private bool isRotating;
+	Rigidbody rb;
+
+	void Start () {
+		sensitivity = 1f;
+		rotation = Vector3.zero;
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void Update () {
+		if (isRotating) {
+			mouseOffset = (Input.mousePosition - mouseReference);
+
+			rotation.z = -(mouseOffset.x + mouseOffset.x) * sensitivity;
+			rb.AddTorque (Vector3.up * rotation.z);
+
+			mouseReference = Input.mousePosition;
+		}
+	}
+
+	void OnMouseDown () {
+		isRotating = true;
+		mouseReference = Input.mousePosition;
+	}
+
+	void OnMouseUp() {
+		isRotating = false;
+	}
+}
---
+++ AR Trial/Assets/Vuforia/Scripts/SwapModel.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class SwapModel : MonoBehaviour {
+
+	public Button btnChanger;
+	public GameObject starryNight;
+	public GameObject almondBlossoms;
+	public GameObject irises;
+
+	// Use this for initialization
+	void Start () {
+		starryNight.SetActive (true);
+		almondBlossoms.SetActive (false);
+		irises.SetActive (false);
+
+		Button btn = btnChanger.GetComponent<Button> ();
+		btn.onClick.AddListener (SetModel);
+	}
+
+	public void SetModel() {
+		string btnName = EventSystem.current.currentSelectedGameObject.name;
+
+		Debug.Log ("Button: "+btnName);
+
+		switch (btnName) {
+		case "StarryNight":
+			starryNight.SetActive (true);
+			almondBlossoms.SetActive (false);
+			irises.SetActive (false);
+			break;
+		case "AlmondBlossoms":
+			starryNight.SetActive (false);
+			almondBlossoms.SetActive (true);
+			irises.SetActive (false);
+			break;
+		case "Irises":
+			starryNight.SetActive (false);
+			almondBlossoms.SetActive (false);
+			irises.SetActive (true);
+			break;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
